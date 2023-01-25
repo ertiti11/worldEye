c6a81fd94d2bc1c08561e6277cdfbdcba00d6653
ertiti11 <vatitiprieto11@gmail.com>
2023-01-25 09:07:43 +0100

    dasta added

--- meili.py
+++ meili.py
@@ -5,5 +5,5 @@ client = meilisearch.Client('http://localhost:7700')
 
 json_file = open('ipss.json')
 ip = json.load(json_file)
-client.index('topCustom').add_documents(ip)
+client.index('maxports').add_documents(ip)
  
---
+++ pruebajson.py
@@ -0,0 +1,27 @@
+import json
+a_file = open("maxports.json", "r")
+json_object = json.load(a_file)
+
+count = 0
+data =[]
+for i in json_object:
+
+    dict = json_object[count]
+    dict["id"] = count
+    a_file.close()
+    # json_object = json.dumps(dict)
+    #json_object = json.dumps(dict, indent=4)ç
+    data.append(dict) 
+    
+    count +=1
+json_string = json.dumps(data)
+
+with open('ipss.json', 'w') as outfile:
+    outfile.write(json_string)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict["color"] = "red"
+# print(thisdict)
